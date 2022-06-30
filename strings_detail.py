b8f8fef68038cd985c897771294d1aede728b432
Hemanth Krishna Nanda <107603676+hemanth365@users.noreply.github.com>
2022-06-30 11:01:39 +0530

    Add files via upload

---
+++ strings_detail.py
@@ -0,0 +1,86 @@
+# x='mentos'
+# print(x)
+# print(x[0]+x[1]+x[2]+x[3]+x[4]+x[5])
+# print(x[1]+x[2]+x[3]+x[4]+x[5])
+# print(x[3]+x[4]+x[5])
+# print(x[5])
+
+# <------------------------------SLICING CONCEPT---------------------------------->
+
+# Y='COMPLIMENT'
+# print(Y[0:10])
+# print(Y[0:9])
+# print(Y[0:8])
+# print(Y[0:7])
+# print(Y[0:6])
+# print(Y[0:5])
+# print(Y[5:4])        #blankline : since the left side value is greater than right side value
+# print(Y[6:10])
+# print(Y[5:10])
+# print(Y[9:10])
+# print(Y[7:10])
+# print(Y[4:10])
+# print(Y[1:10])
+
+Z='tremondous'
+print(Z[0:10])
+print(Z[1:8])
+print(Z[9:10])
+print(Z[9])
+print(Z[4:5])
+print(Z[5:5])    #blankline
+print(Z[1:5])
+print(Z[2:6])
+print(Z[6:8])    #blankline
+print(Z[11])     #indexerror
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
